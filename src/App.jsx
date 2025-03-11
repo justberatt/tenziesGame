e1b5a807aa0fba86799d066bf33072664046a986
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-11 22:34:14 +0100

    imported the Die.jsx component and included it inside the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import Die from './components/Die'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <main>
+        <div className="dice-container">
+          <Die value={5}/>
+          <Die value={3}/>
+          <Die value={2}/>
+          <Die value={3}/>
+          <Die value={4}/>
+          <Die value={2}/>
+          <Die value={1}/>
+          <Die value={6}/>
+          <Die value={3}/>
+          <Die value={4}/> 
+        </div>
       </main>
     </>
   )

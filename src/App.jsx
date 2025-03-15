2aeb9c6e2dcff422bc028c7becbbaa0a68464d52
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-15 23:57:37 +0100

    created a generateAllNewDice() function which will generate 10 random numbers from 1-6 and we are going to use that to create the 10 dice

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import Die from './components/Die'
 
 function App() {
 
+  const generateAllNewDice = () => {
+    let arr = []
+    for (let i=0; i<10; i++) {
+      arr.push(Math.floor(Math.random() * 6 + 1))
+    }
+    return arr
+    // Another way to do the above task
+    //return new Array(10)
+    // .fill(0)
+    //  .map(() => Math.ceil(Math.random() * 6))
+  }
+
   return (
     <>
       <main>

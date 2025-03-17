ea2804815d2121e960d52f95e997ac7e2903b4ff
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-18 00:00:06 +0100

    passed in the id to the function so that the die component is in charge of every state

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,9 +1,9 @@
-const Die = ({value, onClick, isHeld}) => {
+const Die = ({id, value, onClick, isHeld}) => {
   const styles = {
     backgroundColor: isHeld ? "#59E391" : "white"
   }
   return (
-    <button style={styles} onClick={onClick} className="die">{value}</button>
+    <button style={styles} onClick={onClick(id)} className="die">{value}</button>
   )
 }
 
